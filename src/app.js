3a064e81c0a8720fe8b5ba6ba69242465a2a265e
Hakan Budak <hakanbudk0@gmail.com>
2021-10-19 23:40:10 +0300

    19 ekim

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Merhaba Hakan")
+
+
+let dolarDun = 9.30
+let dolarBugun =9.20
+
+const euroBugun=11.2
+console.log(dolarBugun)
+console.log(euroBugun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Kamu Kredisi","Özel Kredisi"]
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length ;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri)
